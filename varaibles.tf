08ac372a9b5f86c67655c312fd5e4e0a43f53127
Zebo Alieva <zeboalieva@air-zebo.mynetworksettings.com>
2023-11-19 18:04:22 -0500

    updated

---
+++ varaibles.tf
@@ -0,0 +1,28 @@
+variable "security_groups" {
+  description = "A map of security groups with their rules"
+  type = map(object({
+    description = string
+    ingress_rules = optional(list(object({
+      description = optional(string)
+      priority    = optional(number)
+
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+    })))
+    egress_rules = optional(list(object({
+      description = optional(string)
+      priority    = optional(number)
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+    })))
+  }))
+  default = {}
+}
+
+variable "vpc_id" {
+  type = string
+}
